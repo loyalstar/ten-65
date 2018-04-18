bc6bde5e94b90e256f3a2e09904c943e60049145
Loyal Star <loyalstar@protonmail.com>
2018-04-18 07:52:16 +0700

    testing1

---
+++ js/mockup/data.js
@@ -0,0 +1,136 @@
+{
+	'order':[
+	{'mode':'LTL',    'status':'Active',   'ten65-id':'123', 'sale-order':'1234456', 'carrier':'Old Carrier', 'origin':'Los Angels','customer':'Jhone Davis', 'destination':'', 'order-status':'Intransit', 'date':'05/07/2016'},
+	{'mode':'Van',    'status':'Active',   'ten65-id':'456', 'sale-order':'1232456', 'carrier':'New Carrier', 'origin':'Chicago',   'customer':'Joseph',      'destination':'', 'order-status':'Intransit', 'date':'04/08/2016'},
+	{'mode':'Van',    'status':'Inactive', 'ten65-id':'458', 'sale-order':'1234536', 'carrier':'Old Carrier', 'origin':'Los Angels','customer':'Jhone Davis', 'destination':'', 'order-status':'Intransit', 'date':'07/07/2015'},
+	{'mode':'Van',    'status':'Inactive', 'ten65-id':'254', 'sale-order':'1523456', 'carrier':'Old Carrier', 'origin':'Chicago',   'customer':'James Jamie', 'destination':'', 'order-status':'Intransit', 'date':'04/12/2016'},
+	{'mode':'LTL',    'status':'Active',   'ten65-id':'125', 'sale-order':'1234956', 'carrier':'New Carrier', 'origin':'Los Angels','customer':'Jhone Davis', 'destination':'', 'order-status':'Intransit', 'date':'07/23/2016'},
+	{'mode':'Flatbed','status':'Active',   'ten65-id':'15',  'sale-order':'1273456', 'carrier':'Old Carrier', 'origin':'Los Angels','customer':'Jhone Davis', 'destination':'', 'order-status':'Intransit', 'date':'04/07/2014'},
+	{'mode':'Flatbed','status':'Active',   'ten65-id':'45',  'sale-order':'1263456', 'carrier':'Old Carrier', 'origin':'Chicago',   'customer':'Joseph',      'destination':'', 'order-status':'Intransit', 'date':'04/07/2016'},
+	{'mode':'LTL',    'status':'Inactive', 'ten65-id':'153', 'sale-order':'1234556', 'carrier':'New Carrier', 'origin':'Chicago',   'customer':'Jhone Davis', 'destination':'New York', 'order-status':'Intransit', 'date':'04/06/2016'},
+	{'mode':'Flatbed','status':'Active',   'ten65-id':'154', 'sale-order':'1233456', 'carrier':'Old Carrier', 'origin':'Los Angels','customer':'James Jamie', 'destination':'Alaska', 'order-status':'Intransit', 'date':'04/12/2016'},
+	{'mode':'Van',    'status':'Inactive', 'ten65-id':'158', 'sale-order':'1223456', 'carrier':'Old Carrier', 'origin':'Chicago',   'customer':'Jhone Davis', 'destination':'Alaska', 'order-status':'Intransit', 'date':'04/23/2016'},
+	{'mode':'Flatbed','status':'Active',   'ten65-id':'451', 'sale-order':'1231456', 'carrier':'Old Carrier', 'origin':'Chicago',   'customer':'Jhone Davis', 'destination':'New York', 'order-status':'Intransit', 'date':'01/07/2016'},
+	{'mode':'LTL',    'status':'Active',   'ten65-id':'156', 'sale-order':'1234056', 'carrier':'New Carrier', 'origin':'Chicago',   'customer':'Jhone Davis', 'destination':'Alaska', 'order-status':'Intransit', 'date':'12/07/2016'},
+	{'mode':'Bulk',   'status':'Active',   'ten65-id':'745', 'sale-order':'1923456', 'carrier':'Old Carrier', 'origin':'Chicago',   'customer':'Jhone Davis', 'destination':'New York', 'order-status':'Intransit', 'date':'34/07/2016'},
+	{'mode':'Van',    'status':'Inactive', 'ten65-id':'158', 'sale-order':'1234856', 'carrier':'Old Carrier', 'origin':'Chicago',   'customer':'James Jamie', 'destination':'Alaska', 'order-status':'Intransit', 'date':'04/23/2006'},
+	{'mode':'Bulk',   'status':'Inactive', 'ten65-id':'721', 'sale-order':'1273456', 'carrier':'New Carrier', 'origin':'Chicago',   'customer':'Jhone Davis', 'destination':'New York', 'order-status':'Intransit', 'date':'04/07/2014'},
+	{'mode':'LTL',    'status':'Inactive', 'ten65-id':'159', 'sale-order':'1236456', 'carrier':'Old Carrier', 'origin':'Los Angels','customer':'Jhone Davis', 'destination':'Tokyo', 'order-status':'Intransit', 'date':'04/07/2017'},
+	{'mode':'Flatbed','status':'Active',   'ten65-id':'753', 'sale-order':'1253456', 'carrier':'Old Carrier', 'origin':'Chicago',   'customer':'Joseph',      'destination':'Alaska', 'order-status':'Intransit', 'date':'04/02/2017'},
+	{'mode':'Van',    'status':'Active',   'ten65-id':'324', 'sale-order':'1233456', 'carrier':'Old Carrier', 'origin':'Chicago',   'customer':'Jhone Davis', 'destination':'New York', 'order-status':'Intransit', 'date':'04/06/2016'},
+	{'mode':'LTL',    'status':'Active',   'ten65-id':'325', 'sale-order':'1234256', 'carrier':'New Carrier', 'origin':'Los Angels','customer':'Jhone Davis', 'destination':'Alaska', 'order-status':'Intransit', 'date':'04/07/2016'},
+	{'mode':'Bulk',   'status':'Active',   'ten65-id':'395', 'sale-order':'1923456', 'carrier':'Old Carrier', 'origin':'Chicago',   'customer':'Jhone Davis', 'destination':'Tokyo', 'order-status':'Intransit', 'date':'04/27/2017'},
+	{'mode':'Van',    'status':'Active',   'ten65-id':'384', 'sale-order':'1237456', 'carrier':'New Carrier', 'origin':'Chicago',   'customer':'Jhone Davis', 'destination':'Tokyo', 'order-status':'Intransit', 'date':'12/07/2016'},
+	{'mode':'Bulk',   'status':'Active',   'ten65-id':'361', 'sale-order':'12536456', 'carrier':'Old Carrier', 'origin':'Chicago',  'customer':'Jhone Davis', 'destination':'Alaska', 'order-status':'Intransit', 'date':'04/07/2016'},
+	{'mode':'LTL',    'status':'Active',   'ten65-id':'328', 'sale-order':'1233456', 'carrier':'New Carrier', 'origin':'Chicago',   'customer':'Jhone Davis', 'destination':'New York', 'order-status':'transit', 'date':'04/12/2016'},
+	{'mode':'Bulk',   'status':'Active',   'ten65-id':'954', 'sale-order':'1223456', 'carrier':'Old Carrier', 'origin':'Los Angels','customer':'Jhone Davis', 'destination':'Tokyo', 'order-status':'transit', 'date':'04/07/2016'},
+	{'mode':'Van',    'status':'Active',   'ten65-id':'854', 'sale-order':'1235456', 'carrier':'Old Carrier', 'origin':'Los Angels','customer':'Joseph',      'destination':'New York', 'order-status':'transit', 'date':'04/07/2016'},
+	{'mode':'LTL',    'status':'Active',   'ten65-id':'146', 'sale-order':'1232456', 'carrier':'New Carrier', 'origin':'Chicago',   'customer':'Joseph',      'destination':'New York', 'order-status':'transit', 'date':'04/07/2016'},
+	{'mode':'Flatbed','status':'Active',   'ten65-id':'748', 'sale-order':'1323456', 'carrier':'New Carrier', 'origin':'Los Angels','customer':'James Jamie', 'destination':'Tokyo', 'order-status':'transit', 'date':'23/07/2016'},
+	{'mode':'LTL',    'status':'Active',   'ten65-id':'541', 'sale-order':'12342356', 'carrier':'New Carrier', 'origin':'Chicago',  'customer':'Jhone Davis', 'destination':'New York', 'order-status':'transit', 'date':'04/07/2016'},
+	{'mode':'LTL',    'status':'Active',   'ten65-id':'545', 'sale-order':'12323456', 'carrier':'New Carrier', 'origin':'Chicago',  'customer':'Jhone Davis', 'destination':'Alaska', 'order-status':'transit', 'date':'04/07/2016'},
+	{'mode':'LTL',    'status':'Active',   'ten65-id':'654', 'sale-order':'1923456', 'carrier':'New Carrier', 'origin':'Chicago',   'customer':'Joseph',      'destination':'Alaska', 'order-status':'transit', 'date':'04/07/2016'},
+	{'mode':'LTL',    'status':'Active',   'ten65-id':'159', 'sale-order':'1234856', 'carrier':'New Carrier', 'origin':'Los Angels','customer':'Jhone Davis', 'destination':'New York', 'order-status':'transit', 'date':'12/07/2016'},
+	{'mode':'LTL',    'status':'Active',   'ten65-id':'756', 'sale-order':'1243456', 'carrier':'New Carrier', 'origin':'Chicago',   'customer':'James Jamie', 'destination':'Alaska', 'order-status':'transit', 'date':'04/07/2016'},
+	{'mode':'Flatbed','status':'Active',   'ten65-id':'651', 'sale-order':'1223456', 'carrier':'New Carrier', 'origin':'Chicago',   'customer':'Jhone Davis', 'destination':'New York', 'order-status':'transit', 'date':'04/07/2016'},
+	{'mode':'Van',    'status':'Active',   'ten65-id':'324', 'sale-order':'1235456', 'carrier':'New Carrier', 'origin':'Chicago',   'customer':'Joseph', 	  'destination':'Tokyo', 'order-status':'transit', 'date':'04/07/2016'},
+	{'mode':'Flatbed','status':'Active',   'ten65-id':'855', 'sale-order':'1023456', 'carrier':'New Carrier', 'origin':'Los Angels','customer':'Jhone Davis', 'destination':'New York', 'order-status':'transit', 'date':'04/07/2016'},
+	{'mode':'LTL',    'status':'Active',   'ten65-id':'111', 'sale-order':'1283456', 'carrier':'New Carrier', 'origin':'Los Angels','customer':'Jhone Davis', 'destination':'Tokyo', 'order-status':'transit', 'date':'04/07/2016'},
+	{'mode':'LTL',    'status':'Active',   'ten65-id':'222', 'sale-order':'1234556', 'carrier':'New Carrier', 'origin':'Chicago',   'customer':'Joseph', 	  'destination':'New York', 'order-status':'transit', 'date':'04/07/2016'},
+	{'mode':'Bulk',   'status':'Inactive', 'ten65-id':'333', 'sale-order':'2123456', 'carrier':'New Carrier', 'origin':'Chicago',   'customer':'Jhone Davis', 'destination':'Tokyo', 'order-status':'transit', 'date':'04/07/2016'},
+	{'mode':'Van',    'status':'Inactive', 'ten65-id':'784', 'sale-order':'1723456', 'carrier':'New Carrier', 'origin':'Los Angels','customer':'Jhone Davis', 'destination':'Tokyo', 'order-status':'transit', 'date':'04/07/2016'},
+	{'mode':'Bulk',   'status':'Active',   'ten65-id':'555', 'sale-order':'1235456', 'carrier':'New Carrier', 'origin':'Chicago',   'customer':'Joseph', 	  'destination':'New York', 'order-status':'transit', 'date':'04/07/2016'},
+	{'mode':'Bulk',   'status':'Active',   'ten65-id':'666', 'sale-order':'1232456', 'carrier':'New Carrier', 'origin':'Chicago',   'customer':'Joseph', 	  'destination':'Tokyo', 'order-status':'transit', 'date':'04/07/2016'},
+	{'mode':'Bulk',   'status':'Active',   'ten65-id':'888', 'sale-order':'1223456', 'carrier':'New Carrier', 'origin':'Chicago',   'customer':'Joseph',       'destination':'Tokyo', 'order-status':'transit', 'date':'04/07/2016'},
+	],
+	'carrier':[
+{'mode':'LTL',    'status':'Active',    'carrier':'Take Load Carrier'}  
+{'mode':'Van',    'status':'Active',    'carrier': 'New Carrier'}
+{'mode':'Van',    'status':'Inactive','carrier': 'USA Carrier'}
+{'mode':'Van',    'status':'Inactive', 'carrier': 'New Carrier'}
+{'mode':'LTL',    'status':'Active',  'carrier': 'USA Carrier'}
+{'mode':'Flatbed','status':'Active',  'carrier':'Take Load Carrier'}
+{'mode':'Flatbed','status':'Active',  'carrier': 'USA Carrier'}
+{'mode':'LTL',    'status':'Inactive', 'carrier':'Take Load Carrier'}
+{'mode':'Flatbed','status':'Active',  'carrier':'Take Load Carrier'}
+{'mode':'Van',    'status':'Inactive', 'carrier': 'New Carrier'}
+{'mode':'Flatbed','status':'Active',  'carrier': 'USA Carrier'}
+{'mode':'LTL',    'status':'Active',  'carrier': 'New Carrier'}
+{'mode':'Bulk',   'status':'Active',  'carrier': 'USA Carrier'}
+{'mode':'Van',    'status':'Inactive','carrier':'Take Load Carrier'}
+{'mode':'Bulk',   'status':'Inactive','carrier':'Take Load Carrier'}
+{'mode':'LTL',    'status':'Inactive','carrier': 'USA Carrier'}
+{'mode':'Flatbed','status':'Active',  'carrier':'Take Load Carrier'}
+{'mode':'Van',    'status':'Active',  'carrier':'Take Load Carrier'}
+{'mode':'LTL',    'status':'Active',  'carrier':'Take Load Carrier'}
+{'mode':'Bulk',   'status':'Active',  'carrier': 'Chicago Carrier'}
+{'mode':'Van',    'status':'Active',  'carrier':'Take Load Carrier'}
+{'mode':'Bulk',   'status':'Active',  'carrier': 'USA Carrier'}
+{'mode':'LTL',    'status':'Active',  'carrier':'Take Load Carrier'}
+{'mode':'Bulk',   'status':'Active',  'carrier': 'New Carrier'}
+{'mode':'Van',    'status':'Active',  'carrier': 'USA Carrier'}
+{'mode':'LTL',    'status':'Active',  'carrier':'Take Load Carrier'}
+{'mode':'Flatbed','status':'Active',  'carrier': 'USA Carrier'}
+{'mode':'LTL',    'status':'Active',  'carrier': 'New Carrier'}
+{'mode':'LTL',    'status':'Active',  'carrier':'Take Load Carrier'}
+{'mode':'LTL',    'status':'Active',  'carrier': 'New Carrier'}
+{'mode':'LTL',    'status':'Active',  'carrier':'Take Load Carrier'}
+{'mode':'LTL',    'status':'Active',  'carrier': 'USA Carrier'}
+{'mode':'Flatbed','status':'Active',  'carrier': 'New Carrier'}
+{'mode':'Van',    'status':'Active',  'carrier': 'Chicago Carrier'}
+{'mode':'Flatbed','status':'Active',  'carrier':'Take Load Carrier'}
+{'mode':'LTL',    'status':'Active',  'carrier': 'Chicago Carrier'}
+{'mode':'LTL',    'status':'Active',  'carrier':'Take Load Carrier'}
+{'mode':'Bulk',   'status':'Inactive','carrier': 'New Carrier'}
+{'mode':'Van',    'status':'Inactive', 'carrier': 'Chicago Carrier'}
+{'mode':'Bulk',   'status':'Active',  'carrier':'Take Load Carrier'}
+{'mode':'Bulk',   'status':'Active',  'carrier': 'Chicago Carrier'}
+{'mode':'Bulk',   'status':'Active',  'carrier': 'New Carrier'}
+
+	],
+	'origin': [
+{'customerid':'123', 'customer':'Jhone Davis', 'email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'456', 'customer':'Joseph',     'email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'458', 'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'254', 'customer':'James Jamie','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'125', 'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'15',  'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'45',  'customer':'Joseph',     'email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'153', 'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'154', 'customer':'James Jamie','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'158', 'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'451', 'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'156', 'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'745', 'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'158', 'customer':'James Jamie','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'721', 'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'159', 'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'753', 'customer':'Joseph',     'email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'324', 'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'325', 'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'395', 'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'384', 'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'361', 'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'328', 'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'954', 'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'854', 'customer':'Joseph',     'email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'146', 'customer':'Joseph',     'email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'748', 'customer':'James Jamie','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'541', 'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'545', 'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'654', 'customer':'Joseph',     'email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'159', 'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'756', 'customer':'James Jamie','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'651', 'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'324', 'customer':'Joseph', 	 'email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'855', 'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'111', 'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'222', 'customer':'Joseph', 	 'email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'333', 'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'784', 'customer':'Jhone Davis','email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'555', 'customer':'Joseph', 	 'email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'666', 'customer':'Joseph', 	 'email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+{'customerid':'888', 'customer':'Joseph',     'email':'JhoneDivis@gmail.com', 'location':'New York','contact-number':'98345612'}
+
+	]
+}
